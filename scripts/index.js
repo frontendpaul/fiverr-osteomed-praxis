68b4ab13df5830b6f76ae485573ebe4b6aa849ff
frontendpaul <matizbrave@gmail.com>
2020-11-04 21:26:34 +0100

    added modal, sliders fot gallery and testimonials, smooth scroll behavior and fixes some small mistakes

--- scripts/index.js
+++ scripts/index.js
@@ -33,30 +33,69 @@ unfoldToggleBtn.addEventListener('click', () => {
 })
 
 
-const formNavigationHandler = function(id) {
+const formNavigationHandler = function (id, parentId) {
   // Get Id of clicked element and parse integer of it
   const idNumber = parseInt(id.match(/\d+/));
   // Toggle visibility of current slide
-  document.querySelector(`.contact-slide-${idNumber}`).classList.toggle('is-active');
+  document.querySelector(`#${parentId} .contact-slide-${idNumber}`).classList.toggle('is-active');
   // Toggle visibility of next slide
-  document.querySelector(`.contact-slide-${idNumber+1}`).classList.toggle('is-active');
+  document.querySelector(`#${parentId} .contact-slide-${idNumber+1}`).classList.toggle('is-active');
   // Toggle fill effect of paginator
-  document.querySelector(`.paginator-item-${idNumber}`).classList.toggle('is-filled');
+  document.querySelector(`#${parentId} .paginator-item-${idNumber}`).classList.toggle('is-filled');
   // Toggle marking fo paginator
-  document.querySelector(`.paginator-item-${idNumber+1}`).classList.toggle('is-active');
+  document.querySelector(`#${parentId} .paginator-item-${idNumber+1}`).classList.toggle('is-active');
 }
 
 // Next Form Button Handler
 const formNextBtns = document.querySelectorAll('.js-form-btn-next');
 for (const formNextBtn of formNextBtns) {
   formNextBtn.addEventListener('click', (e) => {
-    formNavigationHandler(e.target.id);
+    const parentId = e.target.closest('.contact-container').id;
+    formNavigationHandler(e.target.id, parentId);
   });
 }
 // Previous Form Button Handler
 const formPrevBtns = document.querySelectorAll('.js-form-btn-prev');
 for (const formPrevBtn of formPrevBtns) {
   formPrevBtn.addEventListener('click', (e) => {
-    formNavigationHandler(e.target.id);
+    const parentId = e.target.closest('.contact-container').id;
+    formNavigationHandler(e.target.id, parentId);
+  });
+}
+
+
+
+const modalContainer = document.querySelector('.modal-container');
+const ctaBtns = document.querySelectorAll('.js-cta-btn');
+const closeBtns = document.querySelectorAll('.close-btn');
+
+function openModal() {
+  modalContainer.classList.toggle('is-active');
+
+  // This prevents the body to be scrollable
+  let scrollBarWidth = window.innerWidth - document.body.offsetWidth;
+  document.body.style.margin = '0px ' + scrollBarWidth + 'px 0px 0px';
+
+  document.body.style.overflow = 'hidden';
+}
+
+function closeModal() {
+  modalContainer.classList.toggle('is-active');
+
+  document.body.style.margin = '';
+  document.body.style.overflow = '';
+}
+
+for (const ctaBtn of ctaBtns) {
+  ctaBtn.addEventListener('click', () => {
+    openModal(); 
+    console.log('open');
+  });
+}
+
+for (const closeBtn of closeBtns) {
+  closeBtn.addEventListener('click', () => {
+    closeModal();
+    console.log('close');
   });
 }
